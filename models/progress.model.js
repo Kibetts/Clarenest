93f6b9de3ba2d9d21a0f6b1a4bdb5dc22b5b4f9b
Brian Kibet <kibetsbrian@gmail.com>
2024-10-05 13:03:03 +0300

    reformat progress model

--- models/progress.model.js
+++ models/progress.model.js
@@ -1,9 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const progressSchema = new mongoose.Schema({
-    student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
-    courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
-    completion: { type: Number, default: 0 }
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Student",
+    required: true,
+  },
+  class: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Class",
+    required: true,
+  },
+  courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+  assignmentsCompleted: {
+    type: Number,
+    default: 0,
+  },
+  gradeAverage: {
+    type: Number,
+    min: 0,
+    max: 100,
+    default: 0,
+  },
+  remarks: {
+    type: String,
+    maxlength: 250,
+  },
 });
 
-module.exports = mongoose.model('Progress', progressSchema);
+const Progress = mongoose.model("Progress", progressSchema);
+module.exports = Progress;
