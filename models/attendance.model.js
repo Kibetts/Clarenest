9ce63e49490c99bce9f9a98a8ada684acbb6e4eb
Brian Kibet <kibetsbrian@gmail.com>
2024-10-05 12:44:53 +0300

    reformat attendance model

--- models/attendance.model.js
+++ models/attendance.model.js
@@ -1,10 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const attendanceSchema = new mongoose.Schema({
-    student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
-    class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
-    status: { type: String, enum: ['Present', 'Absent'], required: true },
-    date: { type: Date, required: true }
-});
+const attendanceSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+    class: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Class",
+      required: true,
+    },
+    status: { type: String, enum: ["Present", "Absent"], required: true },
+    date: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: (value) => value <= new Date(),
+        message: "Attendance date cannot be in the future",
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Attendance', attendanceSchema);
+const Attendance = mongoose.model("Attendance", attendanceSchema);
+module.exports = Attendance;
