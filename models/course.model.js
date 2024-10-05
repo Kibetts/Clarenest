5b8635793be1a6c87ea0256dafbb8314239165bf
Brian Kibet <kibetsbrian@gmail.com>
2024-10-05 13:41:00 +0300

    reformat course model

--- models/course.model.js
+++ models/course.model.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const courseSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String },
-    classes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Class' }],
-    tutors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tutor' }]
+  title: { type: String, required: true },
+  description: { type: String },
+  classes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Class" }],
+  tutor: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tutor" }],
+  duration: {
+    type: Number,
+    required: [true, "Course duration is required"],
+    min: [1, "Duration must be at least 1 hour"],
+  },
 });
 
-module.exports = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
+module.exports = Course;
