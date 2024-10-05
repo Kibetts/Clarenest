196b6e87ecdc36a2c445f4cda5c1c7ca092cd0d0
Brian Kibet <kibetsbrian@gmail.com>
2024-10-05 13:06:29 +0300

    update & reformat result model

--- models/result.model.js
+++ models/result.model.js
@@ -1,9 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const resultSchema = new mongoose.Schema({
-    student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
-    class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true },
-    grades: [{ assignment: { type: mongoose.Schema.Types.ObjectId, ref: 'Assignment' }, grade: { type: String } }]
-});
+const resultSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+    class: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Class",
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: [true, "Subject is required"],
+    },
+    examDate: {
+      type: Date,
+      required: [true, "Exam date is required"],
+    },
+    grades: [
+      {
+        assignment: { type: mongoose.Schema.Types.ObjectId, ref: "Assignment" },
+        grade: { type: String },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Result', resultSchema);
+const Result = mongoose.model("Result", resultSchema);
+module.exports = Result;
